4a6352832a3b23c5daf6e99e5232ea8d9ba9c249
Ernani1977 <ernani_carparts@hotmail.com>
2026-05-07 22:17:18 -0300

    conexão com banco de dados

---
+++ database.py
@@ -0,0 +1,20 @@
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
+
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+postgres_url = "postgresql+psycopg://postgres:postgres@localhost:5433/tasks"
+engine = create_engine(postgres_url)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
